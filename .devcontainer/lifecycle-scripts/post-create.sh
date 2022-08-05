4abe40a8d01c6492e8f90fff4ecd22d2a8135690
Maxime Gélinas <mgelinas@softframe.net>
2022-08-05 15:44:02 +0000

    chore: set up dev container

---
+++ .devcontainer/lifecycle-scripts/post-create.sh
@@ -0,0 +1,4 @@
+# Run each time the container is successfully created.
+# More info: https://code.visualstudio.com/docs/remote/devcontainerjson-reference#_lifecycle-scripts
+
+echo 'Running "post-create" lifecycle script...'
---
+++ .devcontainer/lifecycle-scripts/post-start.sh
@@ -0,0 +1,4 @@
+# Run each time the container is successfully started.
+# More info: https://code.visualstudio.com/docs/remote/devcontainerjson-reference#_lifecycle-scripts
+
+echo 'Running "post-start" lifecycle script...'
